ac819b76a100b72521cc9026232c88bc39f92850
chelmerd <chelmerd@student.42wolfsburg.de>
2022-10-11 14:15:02 +0200

    add equal

--- utility.hpp
+++ utility.hpp
@@ -92,6 +92,34 @@ struct enable_if<true, T> {
 	typedef T type
 };
 
+// equal
+
+template <class InputIt1, class InputIt2>
+bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
+{
+	while (first1 != last1)
+	{
+		if (*first1 != *first2)
+			return false;
+		++first1;
+		++first2;
+	}
+	return true;
+}
+
+template <class InputIt1, class InputIt2, class BinaryPredicate>
+bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p)
+{
+	while (first1 != last1)
+	{
+		if (!p(*first1, *first2))
+			return false;
+		++first1;
+		++first2;
+	}
+	return true;
+}
+
 } // namespace ft
 
 #endif // UTILITY_HPP
