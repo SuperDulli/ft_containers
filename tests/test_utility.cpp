2250a982203309a3826fc1e269efb69196d10c32
chelmerd <chelmerd@student.42wolfsburg.de>
2022-10-11 14:45:43 +0200

    add test for equal

--- tests/test_utility.cpp
+++ tests/test_utility.cpp
@@ -1,10 +1,10 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
-#include <iomanip>
 
 #include "../debug_utility.hpp"
 
-#ifdef USE_STL	   // CREATE A REAL STL EXAMPLE
+#ifdef USE_STL		   // CREATE A REAL STL EXAMPLE
 #include <type_traits> // std::is_integral
 namespace ft = std;
 #else
@@ -16,25 +16,85 @@ bool test_is_integral()
 	bool success = true;
 
 #ifdef DEBUG
-    std::cout << std::boolalpha;
+	std::cout << std::boolalpha;
 
-    std::cout << "float: " << ft::is_integral<float>::value << "\n";
-    std::cout << "int: " << ft::is_integral<int>::value << "\n";
-    std::cout << "const int: " << ft::is_integral<const int>::value << "\n";
-    std::cout << "wchar_t: " << ft::is_integral<wchar_t>::value << "\n";
-    std::cout << "unsigned long: " << ft::is_integral<unsigned long>::value << "\n";
-    std::cout << "bool: " << ft::is_integral<bool>::value << "\n";
+	std::cout << "float: " << ft::is_integral<float>::value << "\n";
+	std::cout << "int: " << ft::is_integral<int>::value << "\n";
+	std::cout << "const int: " << ft::is_integral<const int>::value << "\n";
+	std::cout << "wchar_t: " << ft::is_integral<wchar_t>::value << "\n";
+	std::cout << "unsigned long: " << ft::is_integral<unsigned long>::value
+			  << "\n";
+	std::cout << "bool: " << ft::is_integral<bool>::value << "\n";
 	std::cout << std::endl;
 #endif
 
-	success = success & (ft::is_integral<float>::value == false);
-	success = success & (ft::is_integral<int>::value == true);
-	success = success & (ft::is_integral<const int>::value == true);
-	success = success & (ft::is_integral<wchar_t>::value == true);
-	success = success & (ft::is_integral<unsigned long>::value == true);
-	success = success & (ft::is_integral<bool>::value == true);
+	success = success && (ft::is_integral<float>::value == false);
+	success = success && (ft::is_integral<int>::value == true);
+	success = success && (ft::is_integral<const int>::value == true);
+	success = success && (ft::is_integral<wchar_t>::value == true);
+	success = success && (ft::is_integral<unsigned long>::value == true);
+	success = success && (ft::is_integral<bool>::value == true);
 
 	return success;
+}
+
+// helper functions for testing equal
+bool is_palindrome(const std::string& s)
+{
+	return ft::equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
+}
+
+bool test_palindrome(const std::string& s)
+{
+	bool result = is_palindrome(s);
+#ifdef DEBUG
+	std::cout << "\"" << s << "\" " << (result ? "is" : "is not")
+			  << " a palindrome" << std::endl;
+#endif
+	return result;
+}
+
+bool predicate_same_case(const char a, const char b)
+{
+	if (!isalpha(a) || !isalpha(b))
+		return false;
+	return ((isupper(a) && isupper(b)) || (islower(a) && islower(b)));
+}
+
+bool compare_string_case(const std::string& s1, const std::string& s2)
+{
+	return ft::equal(s1.begin(), s1.end(), s2.begin(), predicate_same_case);
+}
+
+bool test_string_case(const std::string& s1, const std::string& s2)
+{
+	bool result = compare_string_case(s1, s2);
+
+#ifdef DEBUG
+	std::cout << "\"" << s1 << "\" and \"" << s2 << "\" "
+			  << (result ? "have" : "dont't have")
+			  << " the same case in all characters (length of first)." << std::endl;
+#endif
+	return result;
+}
+
+bool test_equal()
+{
+	bool result = true;
+
+	result = result && (test_palindrome("radar") == true);
+	result = result && (test_palindrome("lagerregal") == true);
+	result = result && (test_palindrome("hello") == false);
+
+	result = result && (test_string_case("hello", "radar") == true);
+	result = result && (test_string_case("Hello", "Radar") == true);
+	result = result && (test_string_case("HeLlO", "RaDaR") == true);
+	result = result && (test_string_case("Hell", "Radar") == true);
+	result = result && (test_string_case("sweet", "1nice") == false);
+	result = result && (test_string_case("sweet", "Sweet") == false);
+	result = result && (test_string_case("swEeT", "Sweet") == false);
+
+	return result;
 }
 
 bool test_utility()
@@ -43,6 +103,7 @@ bool test_utility()
 
 	std::cout << "-- Test utility --" << std::endl;
 	debug::run_test("is_integral", test_is_integral);
+	debug::run_test("equal", test_equal);
 
 	return success;
 }
